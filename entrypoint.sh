bf00f510ba9d80219b449c8f37fe508fc4245019
nothink <gakongakon@gmail.com>
2021-08-31 09:41:46 +0900

    Signing test for production

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ if [ $DEV ]; then
     echo "DEVEROPMENT SERVER RUNNING..."
     python manage.py runserver 0.0.0.0:8000
 else
+    echo "PRODUCTION SERVER RUNNING..."
     # use uvicorn worker over the gunicorn
     # https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/uvicorn/
     # https://stackoverflow.com/questions/62543342/gunicorn-gevent-workers-vs-uvicorn-asgi
