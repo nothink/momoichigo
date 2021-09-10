2077e47102e7fd47ce4ed415d089e6a7b9d93a8a
nothink <gakongakon@gmail.com>
2021-09-10 18:01:22 +0900

    Add docstrings

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,4 +6,5 @@ from rest_framework.test import APIRequestFactory
 
 @pytest.fixture
 def factory() -> APIRequestFactory:
+    """Fixture for Returning APIRequestFactory."""
     return APIRequestFactory()
