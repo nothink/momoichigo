2aec54ec8c0a76420fce05136fd2b4ef78da49ae
nothink <gakongakon@gmail.com>
2021-09-19 04:12:00 +0900

    Fix issue PYL-R0201 again.

--- tests/app/test_models.py
+++ tests/app/test_models.py
@@ -14,7 +14,8 @@ pytestmark = pytest.mark.django_db
 class TestResource:
     """Test Unit of Resource models."""
 
-    def test_model_has_valid_str(self: TestResource, sources: list[str]) -> None:
+    @staticmethod
+    def test_model_has_valid_str(sources: list[str]) -> None:
         """Test that __str__  and key is valid."""
         url_str = sources[random.randrange(len(sources))]
         url = urlparse(url_str)
