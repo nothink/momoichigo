1574f053c593049ac46af55fa67de43e7bfbbfd0
nothink <gakongakon@gmail.com>
2021-10-10 17:35:33 +0900

    Add logger for Centry

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,12 +8,7 @@ python manage.py createcachetable
 echo "Collectstatic"
 python manage.py collectstatic --no-input
 
-if [ $DEBUG ]; then
-    echo "Debug Server RUNNING..."
-    python manage.py runserver 0.0.0.0:${PORT}
-else
-    echo "Production Server RUNNING..."
-    # use uvicorn worker over the gunicorn
-    # https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/uvicorn/
-    gunicorn --bind :${PORT} --workers 4 momoichigo.asgi:application -k uvicorn.workers.UvicornWorker
-fi
+echo "Server RUNNING..."
+# use uvicorn worker over the gunicorn
+# https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/uvicorn/
+gunicorn --bind :${PORT} --workers 4 momoichigo.asgi:application -k uvicorn.workers.UvicornWorker
