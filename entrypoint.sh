260be71b05e25a133e70a7b15b27204f833b6cd6
nothink (Satoshi Ishii) <gakongakon@gmail.com>
2021-10-11 22:53:40 +0900

    Small fix for gunicorn (#41)
    * Small fix for gunicorn
    
    * Change matrix of pytest
    
    * Typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
-# migrations
-echo "Apply database migrations"
-python manage.py migrate
-echo "Create Cache Table"
-python manage.py createcachetable
-echo "Collectstatic"
-python manage.py collectstatic --no-input
+# # migrations
+# echo "Apply database migrations"
+# python manage.py migrate
 
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('macomaco_hi', 'macomaco_hi@seio.club', 'B85-W63-H83')" | python manage.py shell
+# echo "Create Cache Table"
+# python manage.py createcachetable
+# echo "Collectstatic"
+# python manage.py collectstatic --no-input
 
 echo "Server RUNNING..."
 # use uvicorn worker over the gunicorn
 # https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/uvicorn/
-gunicorn --bind :${PORT} --workers 4 momoichigo.asgi:application -k uvicorn.workers.UvicornWorker
+gunicorn --bind :${PORT} --workers 2 momoichigo.asgi:application -k uvicorn.workers.UvicornWorker
