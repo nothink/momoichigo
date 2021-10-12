1283138e29cfb770951aa0301a386a28d6311f45
nothink <gakongakon@gmail.com>
2021-10-12 17:50:03 +0900

    Add DRF-yasg for swagger

--- momoichigo/app/urls.py
+++ momoichigo/app/urls.py
@@ -1,7 +1,6 @@
 """Urls."""
 from django.urls import include, path
 from rest_framework import routers
-from rest_framework.schemas import get_schema_view
 
 from momoichigo.app import views
 
@@ -14,15 +13,4 @@ router.register(r"resources", views.ResourceViewSet, basename="resource")
 
 urlpatterns = [
     path("", include(router.urls)),
-    # schema
-    path("schema", get_schema_view()),
-    # auth
-    path("auth/", include("dj_rest_auth.urls")),
-    path("auth/registration/", include("dj_rest_auth.registration.urls")),
-    path("auth/twitter/login/", views.TwitterLoginView.as_view(), name="twitter_login"),
-    path(
-        "auth/twitter/connect/",
-        views.TwitterConnectView.as_view(),
-        name="twitter_connect",
-    ),
 ]
